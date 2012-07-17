a43332175cc19d3a4038bed7b6d6b36ff02a7e82
diana <borza_diana@yahoo.com>
2012-07-17 13:02:26 +0300

    opencv headers

---
+++ functional cu ui/opencv_headers.h
@@ -0,0 +1,9 @@
+#ifndef OPENCV_HEADERS_H
+#define OPENCV_HEADERS_H
+
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/calib3d/calib3d.hpp>
+#include<opencv2/legacy/legacy.hpp>
+using namespace cv;
+
+#endif // OPENCV_HEADERS_H
