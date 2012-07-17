6bab2305c7262151baed1afda3d61355e81f2561
diana <borza_diana@yahoo.com>
2012-07-17 13:39:32 +0300

    fara oopencv headers

--- functional cu ui/opencv_headers.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENCV_HEADERS_H
-#define OPENCV_HEADERS_H
-
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/calib3d/calib3d.hpp>
-#include<opencv2/legacy/legacy.hpp>
-using namespace cv;
-
-#endif // OPENCV_HEADERS_H
