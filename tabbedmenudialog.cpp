aabbbce5af893eb7ac03e66559c808d9450775c7
Cristina Precup <cp.cristina.pre.cup@gmail.com>
2012-07-25 16:52:14 +0300

    tabbed menu can now be used for capturing frames either by pressing a button or by setting a capture interval. This has been ported from the testcalibrationdialog

--- tabbedmenudialog.cpp
+++ tabbedmenudialog.cpp
@@ -3,21 +3,29 @@
 
 TabbedMenuDialog::TabbedMenuDialog(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::TabbedMenuDialog)
+    m_ui(new Ui::TabbedMenuDialog)
 {
-    ui->setupUi(this);
+    m_ui->setupUi(this);
     m_fileUtils = new FileUtils();
+    m_imageExtension = "";
+    m_imagePath = "";
+    m_leftImage = "";
+    m_rightImage = "";
+    m_left_image = 0;
+    m_right_image = 0;
+    m_disparity =0 ;
 }
 
 TabbedMenuDialog::~TabbedMenuDialog()
 {
-    delete ui;
+    delete m_ui;
+    delete m_fileUtils;
 }
 
 void TabbedMenuDialog::loadCalibParams()
 {
     QString path = m_fileUtils->loadFile(FileUtils::xmlExtension);
-    this->ui->PathField->setText(path);
+    m_ui->PathField->setText(path);
 
     QFile file(path);
          if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
@@ -25,6 +33,160 @@ void TabbedMenuDialog::loadCalibParams()
 
          while (!file.atEnd()) {
              QByteArray line = file.readLine();
-             this->ui->textBrowser->append(line);
+             m_ui->textBrowser->append(line);
          }
+}
+
+void TabbedMenuDialog::browseImagesPath()
+{
+    FileUtils* fileUtils = new FileUtils();
+
+    QString dirName = fileUtils->openDirectory();
+
+    m_imagePath = dirName;
+
+    m_ui->images_path->setText(dirName);
+
+    delete fileUtils;
+}
+
+void TabbedMenuDialog::loadLeftImage()
+{
+    FileUtils *fileUtils = new FileUtils();
+
+    QString fileName = fileUtils->loadFile(FileUtils::imageExtension);
+
+    m_leftImage = fileName;
+
+    m_ui->left_image_name->setText(fileName);
+
+    delete fileUtils;
+}
+
+void TabbedMenuDialog::loadRightImage()
+{
+    FileUtils *fileUtils = new FileUtils();
+
+    QString fileName = fileUtils->loadFile(FileUtils::imageExtension);
+
+    m_rightImage = fileName;
+
+    m_ui->right_image_name->setText(fileName);
+
+    delete fileUtils;
+}
+
+void TabbedMenuDialog::startCalib()
+{
+    int ok = true;
+    m_numberOfImages = m_ui->no_imagesSpinBox->value();
+    m_imageExtension = m_ui->image_extension->text();
+    m_leftImage = m_ui->left_image_name->text();
+    m_rightImage = m_ui->right_image_name->text();
+    m_imagePath = m_ui->images_path->text();
+
+    if(m_leftImage.compare("")==0)
+        ok = false;
+
+    if(m_rightImage.compare("")==0)
+        ok = false;
+
+    if(m_imageExtension.compare("")==0)
+        ok = false;
+
+    if(m_imagePath.compare("")==0)
+        ok = false;
+
+    if(ok) {
+
+        IplImage* sampleImage = cvLoadImage(m_leftImage.toLocal8Bit().data());
+        m_stereoCalibration.initialize(m_ui->board_widthSpinBox->value(), m_ui->board_heightSpinBox->value(), cvGetSize(sampleImage));
+        cvReleaseImage(&sampleImage);
+
+        for(int i = 1; i <= m_numberOfImages;i++) {
+
+            QString left_image_name = m_imagePath;
+            QString right_image_name = m_imagePath;
+
+            left_image_name.replace(QString("\\"), QString("/") );
+            left_image_name.append(tr("/left"));
+            left_image_name.append(QString("%1").arg(i));
+            left_image_name.append(m_imageExtension);
+
+            right_image_name.replace(QString("\\"), QString("/") );
+            right_image_name.append(tr("/right"));
+            right_image_name.append(QString("%1").arg(i));
+            right_image_name.append(m_imageExtension);
+
+
+            IplImage *leftImage = 0, *rightImage = 0;
+
+            leftImage = cvLoadImage(left_image_name.toLocal8Bit().data(), 1);
+            if(leftImage==NULL) {
+                fprintf(stderr,"ERROR: Could not load left image!\n");
+                exit(1);
+            }
+
+            rightImage = cvLoadImage(right_image_name.toLocal8Bit().data(),1);
+            if(rightImage==NULL) {
+                fprintf(stderr,"ERROR: Could not load right image!\n");
+                exit(1);
+            }
+
+            int corners_ok;
+
+            corners_ok = m_stereoCalibration.addImages(leftImage, rightImage);
+
+            if(corners_ok==2) {
+
+                m_stereoCalibration.getInfoFromImages();
+
+            }
+
+            cvReleaseImage(&leftImage);
+            cvReleaseImage(&rightImage);
+        }
+
+
+        StereoCameraParameters* stereoCameraParameters = m_stereoCalibration.calibrateCameras();
+        m_stereoCalibration.rectifyBouguet(stereoCameraParameters);
+
+        m_leftImage.replace(QString("\\"), QString("/"));
+        m_rightImage.replace(QString("\\"), QString("/"));
+
+        IplImage* left_image_rectified = cvLoadImage(m_leftImage.toLocal8Bit().data(),0);
+        IplImage* right_image_rectified = cvLoadImage(m_rightImage.toLocal8Bit().data(),0);
+
+
+        AdjustBMParam* adjustParam = new AdjustBMParam(this);
+        adjustParam->setStereoCalibration(&m_stereoCalibration);
+        adjustParam->setLeftImage(left_image_rectified);
+        adjustParam->setRightImage(right_image_rectified);
+
+        adjustParam->show();
+
+        m_ui->real_time_disp->setEnabled(true);
+    }
+    else {
+
+        fprintf(stderr,"ERROR: Please fill in all the mandatory fields!!!\n");
+        exit(2);
+
+    }
+}
+
+void TabbedMenuDialog::viewDisparityMap()
+{
+    m_ui->real_time_disp->setEnabled(false);
+
+    RTDisparityMapDialog *rtDisparityMapDialog;
+    rtDisparityMapDialog =  new RTDisparityMapDialog(this, this->m_ui->left_camera_index_spinbox->value(), this->m_ui->right_camera_index_spinbox->value());
+    rtDisparityMapDialog->startTimer();
+
+    rtDisparityMapDialog->setStereocalibration(&m_stereoCalibration);
+
+    CvStereoBMState* bmState = cvCreateStereoBMState(0, 64);
+    rtDisparityMapDialog->setBMState(bmState);
+    rtDisparityMapDialog->setImageSize(m_stereoCalibration.getImageSize());
+    rtDisparityMapDialog->show();
 }
--- tabbedmenudialog.h
+++ tabbedmenudialog.h
@@ -2,7 +2,11 @@
 #define TABBEDMENUDIALOG_H
 
 #include <QDialog>
-#include <fileutils.h>
+#include "fileutils.h"
+#include "stereocalibration.h"
+#include "adjustbmparam.h"
+#include"stereocamera.h"
+#include "rtdisparitymapdialog.h"
 
 namespace Ui {
     class TabbedMenuDialog;
@@ -17,10 +21,26 @@ public:
     ~TabbedMenuDialog();
 
 private:
-    Ui::TabbedMenuDialog *ui;
+    Ui::TabbedMenuDialog *m_ui;
     FileUtils *m_fileUtils;
+    QString m_imagePath;
+    QString m_imageExtension;
+    int m_numberOfImages;
+    QString m_leftImage;
+    QString m_rightImage;
+    StereoCalibration m_stereoCalibration;
+    IplImage* m_left_image;
+    IplImage* m_right_image;
+    CvMat* m_disparity;
+
+
 private slots:
     void loadCalibParams();
+    void browseImagesPath();
+    void loadLeftImage();
+    void loadRightImage();
+    void startCalib();
+    void viewDisparityMap();
 };
 
 #endif // TABBEDMENUDIALOG_H
